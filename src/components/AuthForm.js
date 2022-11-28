65caa9e28d01ff8a68019666c62d7f721b2f201e
Kamal Sharma <kamal2507s@gmail.com>
2022-11-29 02:12:12 +0530

    .

--- src/components/AuthForm.js
+++ src/components/AuthForm.js
@@ -1,126 +1,149 @@
-import React, { useState, useRef, useContext } from 'react';
-import { useHistory } from 'react-router-dom';
+import React, { useState, useRef, useContext } from "react";
+import { useHistory } from "react-router-dom";
 
-import classes from './AuthForm.module.css';
-import Add from '../img/addAvatar.png';
-import AuthContext from '../store/auth-store';
+import classes from "./AuthForm.module.css";
+import Add from "../img/addAvatar.png";
+import AuthContext from "../store/auth-store";
 
 import { ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
-import { storage } from '../firebase/firebase';
-
+import { db, storage } from "../firebase/firebase";
+import { doc, setDoc } from "firebase/firestore";
 
 const AuthForm = () => {
-    let idToken;
-    const [showLogin, setShowLogin] = useState(true);
-    const [isLoading, setIsLoading] = useState(false);
-    const [hasError, setHasError] = useState(null);
+  let idToken;
+  const [showLogin, setShowLogin] = useState(true);
+  const [isLoading, setIsLoading] = useState(false);
+  const [hasError, setHasError] = useState(null);
 
-    const displayNameInputRef = useRef();
-    const emailInputRef = useRef();
-    const passwordInputRef = useRef();
-    const history = useHistory();
-    const authCtx = useContext(AuthContext);
+  const displayNameInputRef = useRef();
+  const emailInputRef = useRef();
+  const passwordInputRef = useRef();
+  const history = useHistory();
+  const authCtx = useContext(AuthContext);
 
-    const clickHandler = () => {
-        setShowLogin(prevState => prevState = !prevState);
-    };
+  const clickHandler = () => {
+    setShowLogin((prevState) => (prevState = !prevState));
+  };
 
-    const sendRequest = async (url) => {
-        try {
-            setIsLoading(true);
-            const response = await fetch(url, {
-                method: 'POST',
-                headers: { 'Content-Type': 'application/json' },
-                body: JSON.stringify({
-                    email: emailInputRef.current.value,
-                    password: passwordInputRef.current.value,
-                    returnSecureToken: true
-                }),
-            });
+  const sendRequest = async (url) => {
+    try {
+      setIsLoading(true);
+      const response = await fetch(url, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          email: emailInputRef.current.value,
+          password: passwordInputRef.current.value,
+          returnSecureToken: true,
+        }),
+      });
 
-            setIsLoading(false);
+      setIsLoading(false);
 
-            if (!response.ok) {
-                throw new Error('Something went wrong....');
+      if (!response.ok) {
+        throw new Error("Something went wrong....");
+      }
+
+      const data = await response.json();
+      idToken = data.idToken;
+
+      authCtx.login(idToken);
+
+      history.replace("/home");
+    } catch (error) {
+      setHasError(error.message);
+    }
+  };
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+
+    if (showLogin) {
+      sendRequest(
+        "https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyDJOkOpsUs2msvHNuckU-IXeqd1ff5JwiU"
+      );
+    } else {
+      sendRequest(
+        "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyDJOkOpsUs2msvHNuckU-IXeqd1ff5JwiU"
+      );
+
+      const displayName = displayNameInputRef.current.value;
+      const storageRef = ref(storage, displayName); //second argument is the path where this file will be stored in firebase storage
+
+      uploadBytesResumable(
+        storageRef,
+        document.getElementById("avatar").files[0]
+      ).then(() => {
+        getDownloadURL(storageRef).then(async (downloadURL) => {
+          const response = await fetch(
+            "https://identitytoolkit.googleapis.com/v1/accounts:update?key=AIzaSyDJOkOpsUs2msvHNuckU-IXeqd1ff5JwiU",
+            {
+              method: "POST",
+              headers: {
+                "Content-Type": "application/json",
+              },
+              body: JSON.stringify({
+                idToken: idToken,
+                displayName: displayName,
+                photoUrl: downloadURL,
+                returnSecureToken: true,
+              }),
             }
+          );
+          const userData = await response.json(); //json() will turn this response in json format to a normal javascript object but it returns a promise so we'll have to use await
+          console.log(userData);
 
-            const data = await response.json();
-            idToken = data.idToken;
+          await setDoc(doc(db, "users", userData.localId), {
+            displayName: userData.displayName,
+            email: userData.email,
+            photoUrl: userData.photoUrl,
+            localId: userData.localId,
+          });
+        });
+      });
+    }
+  };
 
-            authCtx.login(idToken);
+  return (
+    <section className={classes["form-container"]}>
+      <h1>Chat App</h1>
+      <span>{`${showLogin ? "Login" : "Register"}`}</span>
+      <form className={classes.form} onSubmit={submitHandler}>
+        {!showLogin && (
+          <input
+            type="text"
+            placeholder="display name"
+            ref={displayNameInputRef}
+          />
+        )}
+        <input type="email" placeholder="email" ref={emailInputRef} />
+        <input type="password" placeholder="password" ref={passwordInputRef} />
+        {!showLogin && (
+          <React.Fragment>
+            <input id="avatar" type="file" style={{ display: "none" }} />
+            <label htmlFor="avatar">
+              <img src={Add} alt="add avatar icon" />
+              <span>Add an avatar</span>
+            </label>
+          </React.Fragment>
+        )}
+        {!isLoading && !hasError && (
+          <button>{`${showLogin ? "Sign In" : "Sign Up"}`}</button>
+        )}
+        {isLoading && <p>Sending Request...</p>}
+        {hasError && <p>{hasError}</p>}
+      </form>
 
-            history.replace('/home');
-        }
-        catch (error) {
-            setHasError(error.message);
-        }
-
-    };
-
-    const submitHandler = event => {
-        event.preventDefault();
-
-        if (showLogin) {
-            sendRequest('https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyDJOkOpsUs2msvHNuckU-IXeqd1ff5JwiU');
-        }
-        else {
-            sendRequest('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyDJOkOpsUs2msvHNuckU-IXeqd1ff5JwiU');
-
-            const displayName = displayNameInputRef.current.value;
-            const storageRef = ref(storage, displayName);
-
-            uploadBytesResumable(storageRef, document.getElementById('avatar').files[0]).then(() => {
-                getDownloadURL(storageRef).then(async (downloadURL) => {
-
-                    const response = await fetch('https://identitytoolkit.googleapis.com/v1/accounts:update?key=AIzaSyDJOkOpsUs2msvHNuckU-IXeqd1ff5JwiU', {
-                        method: 'POST',
-                        headers: {
-                            'Content-Type': 'application/json'
-                        },
-                        body: JSON.stringify({
-                            idToken: idToken,
-                            displayName: displayName,
-                            photoUrl: downloadURL,
-                            returnSecureToken: true
-                        })
-                    });
-                    const data = await response.json(); //json() will turn this response in json format to a normal javascript object
-                    console.log(data);
-                });
-            });
-        }
-    };
-
-    return (
-        <section className={classes['form-container']}>
-            <h1>Chat App</h1>
-            <span>{`${showLogin ? 'Login' : 'Register'}`}</span>
-            <form className={classes.form} onSubmit={submitHandler}>
-                {!showLogin && <input type='text' placeholder='display name' ref={displayNameInputRef} />}
-                <input type='email' placeholder="email" ref={emailInputRef} />
-                <input type='password' placeholder="password" ref={passwordInputRef} />
-                {!showLogin &&
-                    <React.Fragment>
-                        <input id='avatar' type='file' style={{ display: "none" }} />
-                        <label htmlFor='avatar'>
-                            <img src={Add} alt='add avatar icon' />
-                            <span>Add an avatar</span>
-                        </label>
-                    </React.Fragment>
-                }
-                {!isLoading && !hasError && <button>{`${showLogin ? 'Sign In' : 'Sign Up'}`}</button>}
-                {isLoading && <p>Sending Request...</p>}
-                {hasError && <p>{hasError}</p>}
-            </form>
-
-            <div className={classes['toggle-container']}>
-                <span>{`${showLogin ? 'You don\'t have an account?' : 'You do have an account?'}`}</span>
-                <button onClick={clickHandler}>
-                    {`${showLogin ? 'Register' : 'LogIn'}`}
-                </button>
-            </div>
-        </section>
-    );
+      <div className={classes["toggle-container"]}>
+        <span>{`${
+          showLogin ? "You don't have an account?" : "You do have an account?"
+        }`}</span>
+        <button onClick={clickHandler}>
+          {`${showLogin ? "Register" : "LogIn"}`}
+        </button>
+      </div>
+    </section>
+  );
 };
 
 export default AuthForm;
--- src/components/Sidebar/NavBar.js
+++ src/components/Sidebar/NavBar.js
@@ -1,22 +1,54 @@
-import { useContext } from 'react';
-import AuthContext from '../../store/auth-store';
+import { useContext, useEffect, useState } from "react";
+import AuthContext from "../../store/auth-store";
 
-import dp from '../../img/profile-pic.jpg';
-import classes from './NavBar.module.css';
+import dp from "../../img/profile-pic.jpg";
+import classes from "./NavBar.module.css";
 
 const NavBar = () => {
-    const authCtx = useContext(AuthContext);
+  const [currentUser, setCurrentUser] = useState({
+    displayName: null,
+    photoUrl: null,
+  });
 
-    return (
-        <div className={classes.nav}>
-            <span>Chat App</span>
-            <div className={classes['nav_user-info']}>
-                <img src={dp} alt='current user'/>
-                <span>Kamal Sharma</span>
-                <button onClick={() => {authCtx.logout()}}>Logout</button>
-            </div>
-        </div>
-    );
+  const authCtx = useContext(AuthContext);
+
+  useEffect(() => {
+    getCurrentUserData().then(userData => console.log(userData));
+    
+    // setCurrentUser({
+    //   displayName: userData.displayName,
+    //   photoUrl: userData.photoUrl,
+    // });
+  }, []);
+
+  const getCurrentUserData = async () =>
+    await fetch(
+      "https://identitytoolkit.googleapis.com/v1/accounts:lookup?key=AIzaSyDJOkOpsUs2msvHNuckU-IXeqd1ff5JwiU",
+      {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ idToken: authCtx.token }),
+      }
+    )
+      .then((response) => response.json())
+      .then((data) => data.users[0]);
+
+  return (
+    <div className={classes.nav}>
+      <span>Chat App</span>
+      <div className={classes["nav_user-info"]}>
+        <img src={currentUser.photoUrl} alt="current user" />
+        <span>{currentUser.displayName}</span>
+        <button
+          onClick={() => {
+            authCtx.logout();
+          }}
+        >
+          Logout
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default NavBar;
