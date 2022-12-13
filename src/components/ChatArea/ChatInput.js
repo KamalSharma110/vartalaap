18ac9bd38f91ad0f578d6fcbea53a09328d87959
Kamal Sharma <kamal2507s@gmail.com>
2022-12-13 13:39:27 +0530

    .

--- src/components/ChatArea/ChatInput.js
+++ src/components/ChatArea/ChatInput.js
@@ -23,48 +23,54 @@ const ChatInput = () => {
     setImage(event.target.files[0]);
   };
 
+  const emoticonHandler = async() => {
+    const response = await fetch('https://emoji-api.com/emojis?access_key=e08c36abf1b89d65b5a91a915bd063be37fed0e1');
+    const data = await response.json();
+    console.log(data);
+  };
+
   const submitHandler = async (event) => {
     event.preventDefault();
-    if(message === '' && !image) return;
+    if (message === "" && !image) return;
     const chatsRef = doc(db, "chats", chatCtx.combinedId);
-      if (!image) {
-        await updateDoc(chatsRef, {
-          messages: arrayUnion({
-            text: message,
-            senderId: authCtx.currentUserInfo.localId,
-            id: v4(),
-            date: Timestamp.now(), //no matter what we write here,
-            //the date will always be stored in firebase database as a timestamp
-          }),
-        });
-      } else {
-        const storageRef = ref(storage, v4());
-        await uploadBytesResumable(storageRef, image);
-
-        const downloadURL = await getDownloadURL(storageRef);
-
-        await updateDoc(chatsRef, {
-          messages: arrayUnion({
-            text: message,
-            img: downloadURL,
-            senderId: authCtx.currentUserInfo.localId,
-            id: v4(),
-            date: Timestamp.now(),
-          }),
-        });
-      }
-
-      await updateDoc(doc(db, "userChats", authCtx.currentUserInfo.localId), {
-        [chatCtx.combinedId + ".lastMessage"]: message,
-        [chatCtx.combinedId + ".date"]: Timestamp.now(),
+    if (!image) {
+      await updateDoc(chatsRef, {
+        messages: arrayUnion({
+          text: message,
+          senderId: authCtx.currentUserInfo.localId,
+          id: v4(),
+          date: Timestamp.now(), //no matter what we write here,
+          //the date will always be stored in firebase database as a timestamp
+        }),
       });
+    } else {
+      const storageRef = ref(storage, v4());
+      await uploadBytesResumable(storageRef, image);
 
-      await updateDoc(doc(db, "userChats", chatCtx.user.localId), {
-        [chatCtx.combinedId + ".lastMessage"]: message,
-        [chatCtx.combinedId + ".date"]: Timestamp.now(),
+      const downloadURL = await getDownloadURL(storageRef);
+
+      await updateDoc(chatsRef, {
+        messages: arrayUnion({
+          text: message,
+          img: downloadURL,
+          senderId: authCtx.currentUserInfo.localId,
+          id: v4(),
+          date: Timestamp.now(),
+        }),
       });
-      setMessage("");
-      setImage(null);
+    }
+
+    await updateDoc(doc(db, "userChats", authCtx.currentUserInfo.localId), {
+      [chatCtx.combinedId + ".lastMessage"]: message,
+      [chatCtx.combinedId + ".date"]: Timestamp.now(),
+    });
+
+    await updateDoc(doc(db, "userChats", chatCtx.user.localId), {
+      [chatCtx.combinedId + ".lastMessage"]: message,
+      [chatCtx.combinedId + ".date"]: Timestamp.now(),
+    });
+    setMessage("");
+    setImage(null);
   };
 
   return (
@@ -90,9 +96,11 @@ const ChatInput = () => {
 
           {/* <input id='attach-button' type='file' style={{display: 'none'}}/> */}
           <label htmlFor="attach-button">
-            <ion-icon name="attach"></ion-icon>
+            <ion-icon name="happy-outline" onClick={emoticonHandler}></ion-icon>
           </label>
-          <button disabled={image || message !== '' ? false : true}>Send</button>
+          <button disabled={image || message !== "" ? false : true}>
+            Send
+          </button>
         </div>
       </form>
     </div>
