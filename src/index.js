c8b72237cf46270c535d04f3f730aaa2e1f7ac9f
Kamal Sharma <kamal2507s@gmail.com>
2023-01-17 02:08:14 +0530

    .

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import App from "./App";
 
 import "./index.css";
@@ -9,7 +9,7 @@ import { ChatContextProvider } from "./store/chat-context";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <BrowserRouter>
+  <HashRouter>
     <ChatContextProvider>
       <AuthContextProvider>
         <React.StrictMode>
@@ -17,5 +17,5 @@ root.render(
         </React.StrictMode>
       </AuthContextProvider>
     </ChatContextProvider>
-  </BrowserRouter>
+  </HashRouter>
 );
